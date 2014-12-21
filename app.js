da27f254171efa32f545348d3d096c02a92ed2ea
Anthony Barsotti <anthonydbarsotti@gmail.com>
2014-12-21 15:10:46 -0500

    Got API URI on thescore.com for next game in caps' schedule

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var http = require('http');
+var _ = require('underscore');
 var options = {
   host: 'api.thescore.com',
   path: '/nhl/teams/15/events/full_schedule'
 };
 
 var req = http.get(options, function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
-
   var bodyChunks = [];
   res.on('data', function(chunk) {
     bodyChunks.push(chunk);
   }).on('end', function() {
     var body = Buffer.concat(bodyChunks);
-    console.log('BODY: ' + body);
+    var fullSchedule = JSON.parse(body.toString());
+    var nextGame = _.findWhere(fullSchedule, {event_status: 'pre_game'});
+    console.log(nextGame.api_uri);
   })
 });
 
